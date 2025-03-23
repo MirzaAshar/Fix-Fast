323e797d71e75a44b91bc3e735ea88e44bafba58
Mirza Ashar <asherali534@gmail.com>
2025-03-23 19:22:00 +0500

    asad

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "fixfast-production-ad41.up.railway.app";
 
 export const axiosService = axios.create({
     baseURL: BASE_URL,
