8f2de8d41df215d3da58295479eb6071d78bfc2b
Mirza Ashar <asherali534@gmail.com>
2025-03-25 19:59:27 +0500

    Added URL

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "fixfast-production-ad41.up.railway.app";
+export const BASE_URL = "https://fixfast-nine.vercel.app";
 
 export const axiosService = axios.create({
     baseURL: BASE_URL,
